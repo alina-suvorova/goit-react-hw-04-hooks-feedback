ebaa221ed87a29ddfd735d5836eef056a2b8f3f1
Alina <alinasuv@gmail.com>
2022-02-05 22:33:25 +0300

    Добавляет хуки

--- src/components/Feedback.js
+++ src/components/Feedback.js
@@ -4,16 +4,21 @@ import Section from './Section/Section';
 import Notification from './Statistics/Notification';
 import Statistics from './Statistics/Statistics'
 
+
+
 class Feedback extends Component {
+
     state = {
         good: 0,
         neutral: 0,
         bad: 0
       }
-    constructor() {
+      constructor() {
         super();
+    
         this.onLeaveFeedback = this.onLeaveFeedback.bind(this);
       }
+
     onLeaveFeedback = (e) => {
         const {name} = e.target
         this.setState((prev) => ({[name]: prev[name] + 1}))
@@ -25,12 +30,13 @@ class Feedback extends Component {
     countPositiveFeedbackPercentage(good, total) {
        return good / total * 100
     }
-    render(){
+    render() {
         const {good, neutral, bad} = this.state
         const total = this.countTotalFeedback(good, neutral, bad)
         const positivePercentage = this.countPositiveFeedbackPercentage(good, total)
         return (
-            <><Section title="Please leave feedback">
+            <>
+                <Section title="Please leave feedback">
                     <FeedbackOptions options={this.state} onLeaveFeedback={this.onLeaveFeedback} />
                 </Section>
                 <Section title="Statistics">
@@ -42,7 +48,8 @@ class Feedback extends Component {
                         total={total} 
                         positivePercentage={positivePercentage} /> :
                     <Notification message="No feedback given"/> }
-                </Section> </>
+                </Section>
+            </>
         )
     }
 }
